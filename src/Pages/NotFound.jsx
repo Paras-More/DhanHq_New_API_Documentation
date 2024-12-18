8dd42b813175ba6b79713d736cec3b497e121c28
Paras-More <paras@nerve.solutions>
2024-12-18 15:56:43 +0530

    Not found page route changed

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -9,7 +9,7 @@ const NotFound = () => {
       <h1 className="text-4xl font-bold text-red-600 mb-4">404</h1>
       <p className="text-lg text-gray-700 mb-6">Page Not Found</p>
       <button
-        onClick={() => navigate("/")}
+        onClick={() => navigate("/developer")}
         className="px-6 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition"
       >
         Go to Home
